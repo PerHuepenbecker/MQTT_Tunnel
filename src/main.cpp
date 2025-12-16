bb67b2f15c77b2be31c2a6e653b05389f8b15690
perhuepenbecker <perhuepenbecker@googlemail.com>
2025-12-16 11:43:07 +0000

    Updated main.cpp to use CLI11 for arg parsing and C++ classes for tunnel setup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,112 +1,55 @@
-#include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-#include <linux/if.h>
-#include <linux/if_tun.h>
 
-// Function to create a TUN device. 
+#include "TunnelClient.hpp"
+#include "TunnelServer.hpp"
+#include "../third_party/CLI11.hpp"
 
-int tun_create(const char *dev) {
-    struct ifreq ifr;
-    int fd, err;
-
-    // Open the TUN device file
-
-    if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
-        perror("open(/dev/net/tun) failed");
-        return fd;
-    }
-
-    // clear the struct and set the flags
-
-    memset(&ifr, 0, sizeof(ifr));
-    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
-    if (*dev) {
-        strncpy(ifr.ifr_name, dev, IFNAMSIZ);
-    }
-
-    // Configure the TUN device
-
-    if ((err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0) {
-        perror("ioctl(TUNSETIFF) failed");
-        close(fd);
-        return err;
-    }
-
-    return fd;
-}
-
-int tun_fd_static = -1;
-
+enum Mode {
+    MODE_CLIENT,
+    MODE_SERVER
+};
 
 int main(int argc, char** argv) {
 
-    // definition of command line argument variables
-    
-    int opt;
-    char* broker_address = NULL;
-    char* client_id = NULL;
-    char* outbound_topic = NULL;
-    char* inbound_topic = NULL;
-    char* own_address = NULL;
-    char* dst_address = NULL;
-
-    const char *optstring = "s:d:a:c:o:i:";
-
     // parse the command line arguments
 
-    while ((opt = getopt(argc, argv, optstring)) != -1) {
-        switch (opt) {
-            case 'a':
-                broker_address = optarg;
-                break;
-            case 'c':
-                client_id = optarg;
-                break;
-            case 'o':
-                outbound_topic = optarg;
-                break;
-            case 'i':
-                inbound_topic = optarg;
-                break;
-            case 's':
-                own_address = optarg;
-                break;
-            case 'd':
-                dst_address = optarg;   
-                break;
-            default:
-                std::cerr << "Usage: " << argv[0] << "-s <own_ip_adress> -d <dst_ip_address> -a <broker_address> -c <client_id> -o <outbound_topic> -i <inbound_topic>" << std::endl;
-                return 1;
-        }
-    }
-    
+    CLI::App app{"MQTT Tunnel"};
 
-    const char *tun_name = "tun0";
-    int tun_fd = tun_create(tun_name);
-    if (tun_fd < 0) {
-        std::cerr << "Error creating TUN device" << std::endl;
+    std::string broker_address;
+    std::string client_id;
+    std::string command_channel_name = "mqtt_tunnel/commands";
+    
+    Mode mode = MODE_CLIENT;
+
+    app.add_option("-m,--mode", mode, "Mode: client or server")->required()
+        ->transform(CLI::CheckedTransformer(std::map<std::string, Mode>{{"client", MODE_CLIENT}, {"server", MODE_SERVER}}, CLI::ignore_case));
+    app.add_option("-b,--broker", broker_address, "MQTT Broker Address")->required();
+    app.add_option("-c,--client-id", client_id, "Client ID for MQTT connection")->required();
+    app.add_option("-C,--command-channel", command_channel_name, "MQTT Command Channel Name");
+    
+    CLI11_PARSE(app, argc, argv);
+
+    try {
+        if (mode == MODE_CLIENT) {
+            TunnelClientBuilder builder;
+            builder.set_broker_address(broker_address)
+                   .set_client_base_id(client_id)
+                   .set_command_channel_name(command_channel_name);
+            auto client = builder.build();
+            client->start_tunnel();
+        }
+
+        else if (mode == MODE_SERVER) {
+            TunnelServerBuilder builder;
+            builder.set_broker_address(broker_address)
+                   .set_command_channel_name(command_channel_name);
+            auto server = builder.build();
+            server->start_server();
+            
+        }
+    } catch (const std::exception& ex) {
+        spdlog::error("Exception: {}", ex.what());
         return 1;
     }
-    tun_fd_static = tun_fd;
-
-    // Can be cleaned up to use netlink
- 
-    // declare command strings for setting up TUN device
-    char ip_addr_own[100];
-    char ip_addr_dst[100];
-
-    snprintf(ip_addr_own, sizeof(ip_addr_own), "ip addr add %s/24 dev tun0", own_address);
-    snprintf(ip_addr_dst, sizeof(ip_addr_dst), "ip route add %s dev tun0", dst_address);
-
-    // execute commands to set up TUN device
-
-    system(ip_addr_own);
-    system("ip link set tun0 up");
-    system(ip_addr_dst);    
 
 
     
