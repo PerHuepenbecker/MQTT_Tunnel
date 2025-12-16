7b6e0a990fb8bdcc1c8edb120acf3d3a8b74538c
perhuepenbecker <perhuepenbecker@googlemail.com>
2025-12-16 23:39:59 +0000

    Added info

--- src/TunCallback.hpp
+++ src/TunCallback.hpp
@@ -14,13 +14,13 @@ class TunCallback : public virtual mqtt::callback {
 
         void message_arrived(mqtt::const_message_ptr msg) override {
 
-            spdlog::debug("Message arrived with topic: {}", msg->get_topic());
+            spdlog::info("Message arrived with topic: {}", msg->get_topic());
 
             const std::string& payload = msg->get_payload();
 
             ssize_t bytes_written = write(tun_fd_, payload.data(), payload.size());
 
-            spdlog::debug("Wrote {} bytes to TUN device", bytes_written);
+            spdlog::info("Wrote {} bytes to TUN device", bytes_written);
 
             if (bytes_written < 0) {
                 throw std::system_error(errno, std::generic_category(), "Failed to write to TUN device");
