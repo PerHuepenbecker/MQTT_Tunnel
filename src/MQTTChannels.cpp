0a8a7b06d365adbb29e5545a1d721acaa4533df1
perhuepenbecker <perhuepenbecker@googlemail.com>
2026-01-11 20:28:20 +0000

    Modified for setting callback

--- src/MQTTChannels.cpp
+++ src/MQTTChannels.cpp
@@ -13,8 +13,8 @@ mqtt::async_client& MQTTChannels::get_data_client() {
     return *mqtt_data_client_;
 }
 
-void MQTTChannels::set_tun_callback(int tun_fd) {
-    tun_callback_ = std::make_shared<TunCallback>(tun_fd);
+void MQTTChannels::set_tun_callback(int tun_fd, bool encryption_enabled, CryptoManager& crypto_manager) {
+    tun_callback_ = std::make_shared<TunCallback>(tun_fd, crypto_manager, encryption_enabled);
     mqtt_data_client_->set_callback(*tun_callback_);
 }
 
